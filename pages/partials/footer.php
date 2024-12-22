937956af70f52cca5a5f3af181cf58d965b201b3
eljiah <kamelianisaaa@gmail.com>
2024-12-22 21:19:39 +0700

    update php code

---
+++ pages/partials/footer.php
@@ -0,0 +1,20 @@
+<footer class="bg-dark text-light py-4">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-12">
+            <p>&copy; 2024 CIBOER PASS. All Rights Reserved.</p>
+            <div class="social-links">
+              <a href="#" class="text-light mx-2"
+                ><i class="fab fa-instagram"></i
+              ></a>
+              <a href="#" class="text-light mx-2"
+                ><i class="fab fa-facebook"></i
+              ></a>
+              <a href="#" class="text-light mx-2"
+                ><i class="fab fa-twitter"></i
+              ></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
---
+++ pages/partials/navbar.php
@@ -0,0 +1,54 @@
+<header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+          <a class="navbar-brand" href="#" aria-label="CIBOER PASS Homepage">
+            <div class="row justify-content-center align-items-center">
+              <div class="col">
+                <img
+                  src="assets/logo.png"
+                  alt="CIBOER PASS Logo"
+                  height="40"
+                  loading="lazy"
+                />
+              </div>
+              <div class="col">
+                <h5 class="mb-0">CIBOER PASS</h5>
+              </div>
+            </div>
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html#home">Beranda</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="index.html#services">Layanan</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="index.html#about">Tentang</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#contact">Kontak</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="gallery.html">Galeri</a>
+              </li>
+              <li class="nav-item"></li>
+                <a class="nav-link" href="kupon.html">Kupon</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
